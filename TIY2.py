074634fd473e70210a9b346b4defff505dc40706
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-10-26 13:13:28 -0400

    Finish TIY2 and Fixed TIY1 to be a scatter plot.

--- TIY2.py
+++ TIY2.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
+
 x_values = range(1, 5001)
 y_values = [x**3 for x in x_values]
 
 plt.style.use('dark_background')
 fix, ax = plt.subplots()
 
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, s=10, c=y_values, cmap=plt.cm.Reds)
 
 ax.set_title("Cube", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
