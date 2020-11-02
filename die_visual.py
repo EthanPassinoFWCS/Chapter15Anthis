9457de445e3b609a2e1dfbaac3da332d0e5b9d38
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 13:18:26 -0500

    Added two dice for the program instead of one.

--- die_visual.py
+++ die_visual.py
@@ -4,24 +4,25 @@ from die import Die
 
 # Create a D6
 
-die = Die()  # Creating a six sided die
+die_1 = Die()  # Creating a six sided die
+die_2 = Die()
 
 # Make rolls and store the results in a list.
 results = []
 for roll_num in range(1000):
-    results.append(die.roll())
+    results.append(die_1.roll() + die_2.roll())
 
 # Analyze the results.
 frequencies = []
-for value in range(1, die.num_sides+1):
+for value in range(2, die_1.num_sides+die_2.num_sides+1):
     frequencies.append(results.count(value))
 
 
 # Visualize the results.
-x_values = list(range(1, die.num_sides+1))
+x_values = list(range(2, die_1.num_sides+die_2.num_sides+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {"title": "Result"}
+x_axis_config = {"title": "Result", 'dtick': 1}
 y_axis_config = {"title": "Frequency of Result"}
-my_layout = Layout(title="Results of Rolling one 6 sided dice 1000 times", xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename="d6.html")
+my_layout = Layout(title="Results of Rolling two 6 sided dice 1000 times", xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename="d6_d6.html")
