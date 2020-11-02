2c5549e8c3cc49cd31018b5869344359260da780
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 12:42:06 -0500

    Added more points.

--- rw_visual.py
+++ rw_visual.py
@@ -5,14 +5,14 @@ from random_walk import RandomWalk
 
 while True:
     # Make a random walk
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use("dark_background")
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 
     # Point out the first and last points.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
