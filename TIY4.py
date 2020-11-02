e976089cb2824ba7b6dd65202e54b1a4886e5925
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 12:58:00 -0500

    Finished TIY4

---
+++ TIY4.py
@@ -0,0 +1,60 @@
+from random import choice
+import matplotlib.pyplot as plt
+
+
+class RandomWalk:
+
+    def __init__(self, num_points=5000):
+        self.num_points = num_points
+
+        # All walks start at (0, 0).
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+
+        while len(self.x_values) < self.num_points:
+
+            x_direction = choice([4, -4])
+            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([4, -4])
+            y_distance = choice([0, 1, 2, 3, 4, 5, 6, 7])
+            y_step = y_direction * y_distance
+
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Calc new pos
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+
+
+while True:
+    # Make a random walk
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+
+    # Plot the points in the walk.
+    plt.style.use("dark_background")
+    fig, ax = plt.subplots(figsize=(15, 9))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+    # Point out the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == "n":
+        break
