0782476d147812c4c74dee0c11cde68f3ba95094
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 13:08:43 -0500

    Added looking for frequencies.

--- die_visual.py
+++ die_visual.py
@@ -6,7 +6,13 @@ die = Die()  # Creating a six sided die
 
 # Make rolls and store the results in a list.
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     results.append(die.roll())
 
-print(results)
+# Analyze the results.
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequencies.append(results.count(value))
+
+
+print(frequencies)
